be43f674443f3f4520f2e23d6322989f22709630
stefenem1 <stefenem1@gmail.com>
2026-04-09 21:03:15 +0000

    gg.2 4/9

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//basic bone 2
+// Now we go to pubspec YAML and activate Flutter/Assets
