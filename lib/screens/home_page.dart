c795683d33fcbfb2823a88348b07a501b71f6597
stefenem1 <stefenem1@gmail.com>
2026-04-13 20:47:44 +0000

    gg 4/13 "yaml change"

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,8 +10,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold( 
+    appBar: AppBar(
+      title:
+      Row(
+        children: [
+        Image.asset('Assets/rdplogo.png', height: 80),
+        Text('Daily Planner', style: TextStyle(
+          fontFamily: 'Caveat'),
+        )
+        ],
+      ) 
+      )
+    );
   }
 }
-//basic bone 2
+// basic bone 2
 // Now we go to pubspec YAML and activate Flutter/Assets
